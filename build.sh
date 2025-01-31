7f2b70489cc5f8eaf63e5897d4276089cccc65e5
RVSmooth <riveeks.smooth@gmail.com>
2025-01-31 18:21:02 +0530

    chore: Add python-defaults

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
 git clone https://github.com/RMX1821-dev/local_manifests -b main .repo/local_manifests
 
+# dependencies
+sudo apt update -y
+sudo apt install -y python-defaults
+
 # Sync repos
 /opt/crave/resync.sh
 
