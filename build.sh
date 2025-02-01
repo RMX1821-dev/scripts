517571392f2edc1d73442673c5818fa433428000
RVSmooth <riveeks.smooth@gmail.com>
2025-02-01 12:37:59 +0530

    refactor: refactor the script

--- build.sh
+++ build.sh
@@ -1,44 +1,57 @@
 #!/bin/bash
 
-# Clean up old manifests and repos
-rm -rf .repo/local_manifests
+############################################################
+#                          ENV VARS                        #
+############################################################
 
-# Initialize repo
-repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-git clone https://github.com/RMX1821-dev/local_manifests -b main .repo/local_manifests
-
-# dependencies
-sudo apt update -y
-sudo apt install -y python-defaults
-
-# Sync repos
-/opt/crave/resync.sh
-
-# Export environment variables
 export BUILD_USERNAME=rvsmooth
 export BUILD_HOSTNAME=crave
 export KBUILD_USERNAME=rvsmooth
 export KBUILD_HOSTNAME=crave
 export TZ=Asia/Kolkata
 
-# Set up the build environment
+############################################################
+#                         FUNCTIONS                        #
+############################################################
+
+function repo_set(){
+	rm -rf .repo/local_manifests
+	repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+	git clone https://github.com/RMX1821-dev/local_manifests -b main .repo/local_manifests
+}
+
+function sync_projects(){
+
+	/opt/crave/resync.sh
+}
+
+function lunch_build(){
+
+	if [[ "$USERDEBUG" == "1" ]]; then
+		lunch lineage_RMX1821-userdebug
+	elif [[ "$USER" == "1" ]]; then
+		lunch lineage_RMX1821-user 
+	else 
+		lunch lineage_RMX1821-userdebug
+	fi
+}
+
+############################################################
+#                         EXECUTION                        #
+############################################################
+
 source build/envsetup.sh
 
-if [[ "$USERDEBUG" == "1" ]]; then
-	lunch lineage_RMX1821-userdebug
-elif [[ "$USER" == "1" ]]; then
-	lunch lineage_RMX1821-user 
-else 
-	lunch lineage_RMX1821-userdebug
-fi
-
-# Clean and build
-#
 if [[ "$CLEAN_BUILD" == "1" ]]; then
+	repo_set
+	sync_projects
+	lunch_build
 	make installclean
+	m bacon
 else
 	echo "Doing dirty build."
+	lunch_build
+	m bacon
 fi
 
-m bacon
 
